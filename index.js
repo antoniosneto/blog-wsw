9722c4094caad0d996da34a8ed7d6d006647a98f
Antonio Neto <antonionetobhz@gmail.com>
2020-01-21 22:34:18 -0300

    add: All articles for the blog

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function createComponent(stringHtml, selector) {
 const footer = `
     <div class="footer__content row">
         <div class="content__left">
-            <img src="./assets/logo-work-sweet-home.png" alt="logo da work sweet work">
-            <p class="content__about-us"><strong>Sobre nós: </strong>Lugar separado para falar qualquer coisa que pode entrar aqui e tudo mais de bom e bla, e talves a nossa empresa. Lugar separado para falar qualquer coisa que pode entrar aqui e tudo mais de bom e bla, e talves a nossa empresa. Lugar separado para falar qualquer coisa que pode entrar aqui e tudo mais de bom e bla, e talves a nossa empresa.</p>
+            <a href="http://blog.wsweetwork.com.br"><img src="http://blog.wsweetwork.com.br/assets/logo-work-sweet-home.png" alt="logo da work sweet work"></a>
+            <p class="content__about-us"><strong>Sobre nós: </strong>Nós somos jovens. Fazemos parte da geração de novos trabalhadores e queremos motivar vocês a fazerem parte desta revolução. Qual a melhor forma de trabalhar? Como conciliar vida pessoal com profissional? Como ser mais produtivo? Como empreender neste mundo louco? Nós estamos aqui para te dar essas respostas e caminhar junto com você nesta descoberta de mundo. Não existem barreiras. Você está pronto?</p>
         </div>
         <div class="content__right">
             <div class="content__social-media">
                 <p><strong>SIGA-NOS NAS REDES SOCIAIS</strong></p>
                 <div class="social-media__img row">
-                    <img src="./assets/instagram.svg" alt="">
-                    <img src="./assets/linkedin.svg" alt="">
+                    <a href="https://www.instagram.com/work.sweet.work/" target="_blank"><img src="http://blog.wsweetwork.com.br/assets/instagram.svg" alt=""></a>
+                    <a href="https://www.linkedin.com/company/worksweetwork/ target="_blank""><img src="http://blog.wsweetwork.com.br/assets/linkedin.svg" alt=""></a>
                 </div>
             </div>
             <p class="about__development"><small>Desenvolvido por: <strong>HD4</strong></small></p>
@@ -23,17 +23,13 @@ const footer = `
 
 const header = `<div class="content row">
         <div class="content__img align-center">
-            <img src="./assets/logo-work-sweet-home.png" alt="logo da work sweet work">
+            <a href="http://blog.wsweetwork.com.br"><img src="http://blog.wsweetwork.com.br/assets/logo-work-sweet-home.png" alt="logo da work sweet work"></a>
             <h1>work sweet work</h1>
         </div>
         <div class="content__menu row align-center">
-            <a href="">visite nosso site</a>
-            <div class="row align-center category">
-                <p>categorias</p>
-                <i></i>
-            </div>
-            <img src="./assets/instagram.svg" alt="nosso instagram">
-            <img src="./assets/linkedin.svg" alt="nosso linkedin">
+            <a href="http://www.wsweetwork.com.br">visite nosso site</a>
+            <a href="https://www.instagram.com/work.sweet.work/" target="_blank"><img src="http://blog.wsweetwork.com.br/assets/instagram.svg" alt="nosso instagram"></a>
+            <a href="https://www.linkedin.com/company/worksweetwork/" target="_blank"><img src="http://blog.wsweetwork.com.br/assets/linkedin.svg" alt="nosso linkedin"></a>
         </div>
     </div>`;
 
--- js/blog.js
+++ js/blog.js
@@ -8,16 +8,19 @@ function getArticles(cards) {
 }
 
 function cardTemplate(card) {
-    return `<div class="content__box">
-            <div class="box__img align-center">
-                <img src="${card.img.src}" alt="${card.img.alt}">
+    return `
+        <a href="${card.href}">
+            <div class="content__box">
+                <div class="box__img align-center">
+                    <img src="${card.img.src}" alt="${card.img.alt}">
+                </div>
+                <div class="box__description">
+                    <h2 class="title">${card.article.title}</h2>
+                    <p class="subtitle">${card.article.subtitle}</p>
+                    <p class="description">${card.article.description}</p>
+                </div>
             </div>
-            <div class="box__description">
-                <h2 class="title">${card.article.title}</h2>
-                <p class="subtitle">${card.article.subtitle}</p>
-                <p class="description">${card.article.description}</p>
-            </div>
-        </div>`;
+        </a>`;
 }
 
 const cards = [
@@ -32,7 +35,8 @@ const cards = [
             description:
                 "A indústria de coworking está crescendo e cada vez mais pessoas procuram um lugar onde possam trabalhar sem estar em casa, em uma cafeteria ou gastando com aluguel de um escritório. Espaços de coworking têm inúmeros benefícios para os membros, como falta...",
             author: "Agathe 15/12"
-        }
+        },
+        href: "./artigos/trabalhando-em-um-coworking.html"
     },
     {
         img: {
@@ -45,7 +49,8 @@ const cards = [
             description:
                 "Há uma crescente tendência de grandes corporações aderirem aos benefícios dos espaços de coworking. Alguns gigantes tecnológicos estão testando o uso de espaços de coworking para os funcionários, para que possam estar próximos e interagir facilmente...",
             author: "Agathe 15/12"
-        }
+        },
+        href: "./artigos/5-razoes-para-usar-um-coworking.html"
     },
     {
         img: {
@@ -58,20 +63,36 @@ const cards = [
             description:
                 "Os espaços coworking são um novo tipo de empreendimento que está em alta. É um modelo jovem, com menos de 10 anos no mercado, possibilita inúmeras formas de geração de renda e destaque, além de trazer remuneração e movimento para os imóveis parados em...",
             author: "Agathe 15/12"
-        }
+        },
+        href: "./artigos/coworking-um-investimento-sustentavel.html"
     },
     {
         img: {
-            src: "",
+            src: "./assets/artigos/viajar-e-trabalhar.png",
             alt: "Imagem a ser exibida"
         },
         article: {
             title: "Viajar e Trabalhar ao Mesmo Tempo?",
-            subtitle: "Com coworking é fácil!",
+            subtitle: "Uma moça feliz indo viajar com seu computador",
             description:
                 "É o sonho de muitos trabalhar de onde quiserem. Enquanto beber um coquetel de coco embaixo de uma palmeira e digitar no seu laptop pode ser logicamente improvável (teclado cheio de areia?), existem maneiras de escapar da vida cotidiana e continuar trabalhando em...",
             author: "Agathe 15/12"
-        }
+        },
+        href: "./artigos/viajar-e-trabalhar-ao-mesmo-tempo.html"
+    },
+    {
+        img: {
+            src: "./assets/artigos/home-office.png",
+            alt: "Uma mesa com café e computadores"
+        },
+        article: {
+            title: "Home Office ou Coworking?",
+            subtitle: "Dúvida cruel!",
+            description:
+                "Hoje não restam dúvidas que as novas gerações dão muito mais valor à liberdade de tempo do que a qualquer outra coisa. Isso, inclusive, é uma característica muito comum dos novos profissionais. Por esse motivo, fazer aquela velha comparação...",
+            author: "Agathe 15/12"
+        },
+        href: "./artigos/home-office-ou-coworking.html"
     }
 ];
 
